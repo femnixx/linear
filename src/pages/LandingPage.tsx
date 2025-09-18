6a3aa3e283bbccfc69343f5070597a6e0bd37edc
femnixx <femnitality@gmail.com>
2025-09-18 14:05:46 +0700

    ,

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -20,8 +20,8 @@ const LandingPage = () => {
         <p> &gt; </p>
           </div></button>
           <p className="mt-13">Image</p>
-        <p>Powering the world's best product teams.</p>
-        <p>From next-gen startups to established enterprises.</p>
+        <p>Powering the world's best product teams. From next-gen startups to established enterprises.</p>
+        
     </div>
   )
 }
