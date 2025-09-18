ffd882977d767dfcc017bae49b3435f6f6edcab4
femnixx <femnitality@gmail.com>
2025-09-18 13:42:50 +0700

    Starting to develop title card

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,14 @@ const LandingPage = () => {
   return (
     <div className="w-screen h-screen">
         <Bar></Bar>
+        {/* title card */}
+        <div>
+          {/* introducing linear heights */}
+          <div className="flex">
+            <p>Introducing Linear Insights</p>
+            <p> -&gt; </p>
+          </div>
+        </div>
     </div>
   )
 }
