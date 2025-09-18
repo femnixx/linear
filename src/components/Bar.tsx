31bd4dc37fc6274468748d1ab2ff60eddcc4da58
femnixx <femnitality@gmail.com>
2025-09-18 13:38:25 +0700

    Added MUI components, react-bits, and GSAP

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -2,20 +2,27 @@
 const Bar = () => {
   return (
     <div className="flex justify-between">
-      {/* main bar */}
-      <div className="flex">
-      <p>Linear</p>
-      <p>Features</p>
-      <p>Method</p>
-      <p>Customers</p>
-      <p>Changelog</p>
-      <p>Integrations</p>
-      <p>Pricing</p>
+      {/* lg screens and above */}
+      <div className="max-lg:hidden">
+        {/* main bar */}
+        <div className="flex">
+        <p>Linear</p>
+        <p>Features</p>
+        <p>Method</p>
+        <p>Customers</p>
+        <p>Changelog</p>
+        <p>Integrations</p>
+        <p>Pricing</p>
+        </div>
+        {/* login and signup */}
+        <div className="flex">
+          Log in
+          <div>
+            Sign up
+          </div>
+        </div>
       </div>
-      {/* login and signup */}
-      <div className="flex">
-
-      </div>
+      
     </div>
   )
 }
