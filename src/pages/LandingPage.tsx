31fee30d5a05bcf38ccb246ffab507c9e301c56c
femnixx <femnitality@gmail.com>
2025-09-19 20:05:03 +0700

    introducing linear insights

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,6 @@
+import Frame from "../assets/Frame.png";
 import Bar from "../components/Bar";
+
 const LandingPage = () => {
   return (
    <>
@@ -8,20 +10,21 @@ const LandingPage = () => {
 
   {/* <!-- Content --> */}
   <Bar></Bar>
-  <div className="flex flex-col justify-center mt-16 text-white">
+  <div className="flex flex-col justify-center mt-10 text-white">
     <div className="w-full flex justify-center items-center">
-      <div className="flex items-center justify-center gap-x-3 bg-white/5 w-fit rounded-full px-3 py-1">
+      <div className="flex items-center justify-center gap-x-3 bg-white/5 w-fit rounded-full pl-3 pr-1.5 py-1">
         <p className="text-xs ">Introducing Linear Insights</p>
-        <div className="rounded-full px-1 bg-white/5 flex items-center justify-center"><p>-&gt;</p></div>
+        <div className="rounded-full px-2.5 bg-white/5 flex items-center justify-center"><p>-&gt;</p></div>
       </div>
     </div>
-    <h1 className="text-4xl font-bold text-center mx-3 mt-6">Linear is a better way to build products</h1>
-    <p className="mt-4 text-gray-300 text-center">Meet the new standard for modern software development.</p>
+    <h1 className="text-3xl font-bold text-center mx-3 mt-6">Linear is a better way to build products</h1>
+    <p className="mt-4 text-gray-300 text-center text-sm">Meet the new standard  for modern <br></br> software development.</p>
     <div className="w-full flex justify-center">
-      <button className=" text-sm w-fit mt-6 px-6 py-3 bg-gradient-to-r from-[#455EB5] via-[#5643CC] to-[#673FD7] rounded-full shadow-lg">
+      <button className=" text-sm w-fit mt-6 px-3 py-2.5 bg-gradient-to-r from-[#455EB5] via-[#5643CC] to-[#673FD7] rounded-full shadow-lg">
         Get Started &gt;
       </button>
     </div>
+    <img src={Frame} alt="frame " className="mt-10 mx-5"/>
   </div>
 </div>
    </>
