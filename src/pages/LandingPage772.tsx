6265ba3fa31d13da21bb424ae40c9ecf0c25c23e
femnixx <femnitality@gmail.com>
2025-09-19 20:51:19 +0700

    Figured out the background

--- src/pages/LandingPage772.tsx
+++ src/pages/LandingPage772.tsx
@@ -1,8 +1,20 @@
 
 const LandingPage772 = () => {
   return (
-    <div className="bg-black w-full h-full">
-            hi
+    <div className="bg-[#000212] w-full h-full">
+        {/* top decoration */}
+        <div className="flex">
+            {/* purple smoke */}
+            <div className="z-5">
+
+            </div>
+            {/* line */}
+            <div className="border-1 w-full rounded-t-lg border-[#7877C666]">
+
+            </div>
+        </div>
+            <p>Unlike any tool you've used before</p>
+            <p>Designed to the last pixel and engineered with unforgiving precision, Linear combines UI elegance with world-class performance.</p>
     </div>
   )
 }
