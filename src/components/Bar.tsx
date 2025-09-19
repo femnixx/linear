535f64012d34c42999f67d574a4041420e96038b
femnixx <femnitality@gmail.com>
2025-09-19 20:30:24 +0700

    Almost done with first page

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -24,12 +24,12 @@ const Bar = () => {
         </div>
       </div>
       {/* sm-md screens */}
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between items-center w-full mt-5 mx-5">
         <button className="hover:cursor-pointer border-white/30 border-2 rounded-full p-1 bg-white/20"><img src={menubutton} alt="menu hamburger" /> </button> 
         {/* login and signin */}
         <div className="gap-x-3 flex">
-        <button>Login</button>
-          <button>Sign up</button>
+        <button className="text-sm">Login</button>
+          <button className="text-sm bg-gradient-to-r from-[#455EB5] via-[#5643CC] to-[#673FD7] px-2 py-1 rounded-full">Sign up</button>
         </div>
       </div>
     </div>
