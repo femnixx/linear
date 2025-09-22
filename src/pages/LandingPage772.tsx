66f2d541eaecc0aebb017e85e2ea5198d65a7fad
femnixx <femnitality@gmail.com>
2025-09-22 10:36:27 +0700

    some stuff for distance

--- src/pages/LandingPage772.tsx
+++ src/pages/LandingPage772.tsx
@@ -39,20 +39,10 @@ const LandingPage772 = () => {
       <div className=" flex flex-col">
             
             {/* keyboard */}
-            <div className="bg-white/7 p-3 mx-5 rounded-xl mt-5 ">
-              <img src={keyboard} alt="" />
-              <div className="flex x-scr gap-x-1 justify-end">
-              <div className="flex pr-1 gap-x-0.5 text-xs bg-white/10 rounded-full px-2">
-                <p className="text-[12px]">K</p>
-                <p className="text-[10px]">Opens command line</p>
-              </div>
-              <div className="flex gap-x-1 pr-1 text-xs bg-white/10 rounded-full px-2">
-                <p className="text-[12px]">I</p>
-                <p className="text-[10px]">Assign issue to me</p>
-              </div>
-              </div>
-              <p>Built for your keyboard</p>
-              <p>Fly through your tasks with rapid-fire keyboards hortcuts for everything. Literally everything.</p>
+            <div className="bg-white/7 p-5 mx-5 rounded-xl mt-5 flex flex-col justify-center items-center text-center">
+              <img src={keyboard} alt="" className="bg-white/10 p-0.5 rounded-lg"/>
+              <p className="font-bold mt-5">Built for your keyboard</p>
+              <p className="text-xs mt-1.5">Fly through your tasks with rapid-fire keyboards hortcuts for everything. Literally everything.</p>
             </div>
             
             {/* lightning */}
